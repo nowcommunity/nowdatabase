c5d95761a764c93c24cccfa3dc631abc287e0bd2
arttu <ttu91@hotmail.com>
2024-02-21 20:04:09 +0200

    Backend: Ts-node-dev and express mvp

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+const PORT = 4000;
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
