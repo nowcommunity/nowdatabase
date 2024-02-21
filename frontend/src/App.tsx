65e29650b93de3ff40be522b25ec78c155ed96de
arttu <ttu91@hotmail.com>
2024-02-21 19:08:10 +0200

    Add vite react TS app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>NOW Database</h1>
+        <p>
+          Hello world
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
