e1f8cb0c04906f267c99d62fb7d541633193a985
ShootingStar91 <ttu91@hotmail.com>
2024-02-22 00:23:02 +0200

    Back&front connection pinging, dev QL improvements

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const PORT = 4000
-
 app.get('/ping', (_req, res) => {
-  res.send('pong')
+  console.log("someone pinged")
+  res.send({ message: 'pong' })
 })
 
 app.listen(PORT, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
 const App = () => {
+  const [text, setText] = useState('___')
+
+  const onClick = () => {
+    fetch('http://localhost:4000/ping').then(response => response.json().then(result => setText(result.message))).catch(e => console.log("Error"))
+  }
+
   return (
     <>
       <div>
         <h1>NOW Database</h1>
         <p>Hello world</p>
+        <button onClick={onClick}>Ping backend</button>
+        <p>Result: {text === 'pong' ? 'Connection to backend works!' : 'Backend has not responded.'}</p>
       </div>
     </>
   )
