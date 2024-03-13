29635a66faa67b85c2c3f1b5e6051885f22faf01
arttu <ttu91@hotmail.com>
2024-03-13 11:26:19 +0200

    Add backend/build to .gitignore

--- backend/build/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const info = (msg) => {
-    // eslint-disable-next-line no-console
-    console.log(msg);
-};
-exports.default = { info };
