ccaa1ef0bb260d9c5f698f4cbf2c3dc7a1c8b84f
arttu <ttu91@hotmail.com>
2024-04-09 13:04:51 +0300

    Persist user-state in localStorage

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, Middleware } from '@reduxjs/toolkit'
 import { userApi, userReducer } from './userReducer'
 
+const localStorageMiddleware: Middleware = store => next => action => {
+  const result = next(action)
+  const state = store.getState()
+  localStorage.setItem('userState', JSON.stringify(state.user))
+  return result
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('userState')
+    if (serializedState === null) {
+      return undefined
+    }
+    const parsedState = JSON.parse(serializedState)
+    return { user: parsedState }
+  } catch (err) {
+    return undefined
+  }
+}
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
     user: userReducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(userApi.middleware),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(userApi.middleware).concat(localStorageMiddleware),
+  preloadedState: loadFromLocalStorage(),
 })
 
 export type RootState = ReturnType<typeof store.getState>
