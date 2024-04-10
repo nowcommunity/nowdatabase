c1eff88119bd7409d048287e08d0d9dc6840e891
arttu <ttu91@hotmail.com>
2024-04-10 19:59:58 +0300

    Add retries of db connection: otherwise fails on first run

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { testDb } from './services/loc_name'
 import { sequelize } from './utils/db'
 import userRouter from './routes/user'
 import { requestLogger } from './utils/requestLogger'
+import { sleep } from './utils/common'
 
 const app = express()
 
@@ -22,8 +23,29 @@ app.get('/ping', (_req, res) => {
 })
 
 const testDbConnection = async () => {
-  await sequelize.authenticate()
-  await testDb()
+  const tryDbConnection = async () => {
+    try {
+      await sequelize.authenticate()
+      await testDb()
+      return true
+    } catch (e) {
+      if (e instanceof Error) logger.error(e.toString())
+      else logger.error('DB connection failed with unknown error type')
+      return false
+    }
+  }
+  const maxAttempts = 20
+  let attempts = 0
+  while (attempts < maxAttempts) {
+    const success = await tryDbConnection()
+    if (success) {
+      return
+    }
+    logger.info(`Trying again in 6 seconds, attempt ${attempts} / ${maxAttempts}`)
+    attempts++
+    await sleep(6000)
+  }
+  logger.error(`Attempted ${maxAttempts} times, but database connection could not be established`)
 }
 
 app.listen(PORT, () => {
