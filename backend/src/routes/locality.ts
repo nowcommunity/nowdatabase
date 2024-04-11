4a9ee70f908926d4376d66348214aac32e1605f5
arttu <ttu91@hotmail.com>
2024-04-11 10:55:18 +0300

    Show table and use actual data from backend

--- backend/src/routes/locality.ts
+++ backend/src/routes/locality.ts
@@ -3,7 +3,7 @@ import { getAllLocalities } from '../services/now_loc'
 
 const router = Router()
 
-router.get('/all', async (req, res) => {
+router.get('/all', async (_req, res) => {
   const localities = await getAllLocalities()
   return res.status(200).send(localities)
 })
--- frontend/src/components/Localities.tsx
+++ frontend/src/components/Localities.tsx
@@ -1,9 +1,53 @@
-import { Box } from '@mui/material'
+import { useMemo } from 'react'
+import { MaterialReactTable, useMaterialReactTable, type MRT_ColumnDef } from 'material-react-table'
+import { useGetAllLocalitiesQuery } from '../redux/localityReducer'
+import { CircularProgress } from '@mui/material'
+
+interface Locality {
+  loc_name: string
+  min_age: number
+  max_age: number
+  country: string
+}
 
 export const Localities = () => {
+  const localitiesQuery = useGetAllLocalitiesQuery({})
+  const columns = useMemo<MRT_ColumnDef<Locality>[]>(
+    () => [
+      {
+        accessorKey: 'loc_name',
+        header: 'Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'min_age',
+        header: 'Min age',
+        size: 150,
+      },
+      {
+        accessorKey: 'max_age',
+        header: 'Max age',
+        size: 150,
+      },
+      {
+        accessorKey: 'country',
+        header: 'Country',
+        size: 150,
+      },
+    ],
+    []
+  )
+
+  const table = useMaterialReactTable({
+    columns,
+    data: localitiesQuery.data ?? [],
+  })
+
+  if (!localitiesQuery.data) return <CircularProgress />
+
   return (
-    <Box>
-      <p>Localities</p>
-    </Box>
+    <div>
+      <MaterialReactTable table={table} />
+    </div>
   )
 }
