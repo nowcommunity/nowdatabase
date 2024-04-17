8de9c49dd67b6b3ea309038c35e87383f4c2f8a0
arttu <ttu91@hotmail.com>
2024-04-17 11:25:15 +0300

    Remove unused LabeledItems

--- frontend/src/components/DetailView/common/FormComponents.tsx
+++ frontend/src/components/DetailView/common/FormComponents.tsx
@@ -46,29 +46,5 @@ export const DataValue = <T extends object>({
   if (mode === 'edit') {
     return getEditElement(field)
   }
-  return <b>{data[field] as ReactNode}</b>
-}
-
-export type LabeledItem = {
-  label: string
-  component?: ReactNode
-}
-
-export const LabeledItems = ({ items }: { items: LabeledItem[] }) => {
-  return (
-    <Box>
-      <Grid direction="column" container rowGap={'0.4em'}>
-        {items.map(item => (
-          <Grid key={item.label} direction="row" container gap="2em" minHeight="2.5em">
-            <Grid xs={4} item>
-              <b>{item.label}</b>
-            </Grid>
-            <Grid xs={4} item>
-              {item.component}
-            </Grid>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
-  )
+  return data[field]
 }
