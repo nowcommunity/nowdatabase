333e3c0072ca4e46d237237a745ffad218662788
karilint <kari.lintulaakso@gmail.com>
2024-04-29 14:09:13 +0300

    Added species retrieval from backend

--- frontend/src/backendTypes.d.ts
+++ frontend/src/backendTypes.d.ts
@@ -1,12 +1,17 @@
 import { type now_locAttributes } from '../../backend/src/models/now_loc'
 import { type com_mlist as Museum } from '../../backend/src/models/com_mlist'
+import { type com_speciesAttributes } from '../../backend/src/models/com_species'
 
 interface Locality extends now_locAttributes {
   museum_com_mlists: Museum;
   synonyms: string[]
 }
 
+interface Species extends com_speciesAttributes {
+}
+
 export {
   Locality,
   Museum,
+  Species,
 }
