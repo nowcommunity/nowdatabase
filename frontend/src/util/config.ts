2ebe43234e4562461ae76845009fd486d67bcf3c
arttu <ttu91@hotmail.com>
2024-04-30 15:12:21 +0300

    Rename env

--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = (import.meta.env.NOWDB_BACKEND_URL as string | undefined) ?? 'http://localhost:4000/'
+export const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL as string | undefined) ?? 'http://localhost:4000/'
