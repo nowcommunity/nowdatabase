a883c857e0e36a7d053fe2bf8fc869b766f70004
arttu <ttu91@hotmail.com>
2024-04-30 10:40:37 +0300

    Remove commented code

--- backend/src/services/species.ts
+++ backend/src/services/species.ts
@@ -1,16 +1,5 @@
-//import { com_mlist } from '../models/com_mlist'
 import { models } from '../utils/db'
-//import { logger } from '../utils/logger'
 
-/*export const testDb = async () => {
-  const result = await models.now_loc.findAll({ attributes: ['loc_name'], where: { loc_name: 'Amba East' }, raw: true })
-  if (result[0].loc_name === 'Amba East') {
-    logger.info('Database connection tested, data can be fetched')
-  } else {
-    logger.error('Database connection does not work')
-  }
-}
-*/
 export const getAllSpecies = async (onlyPublic: boolean) => {
   const where = onlyPublic ? { sp_status: 0 } : {}
 
