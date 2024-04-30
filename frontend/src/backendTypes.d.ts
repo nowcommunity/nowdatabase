dda418c7b9bcea3532349a0394d7df4775a7b0dd
karilint <kari.lintulaakso@gmail.com>
2024-04-30 15:56:03 +0300

    Frontend for References List

--- frontend/src/backendTypes.d.ts
+++ frontend/src/backendTypes.d.ts
@@ -1,10 +1,14 @@
 import { type now_locAttributes } from '../../backend/src/models/now_loc'
 import { type com_mlist as Museum } from '../../backend/src/models/com_mlist'
 import { type com_speciesAttributes } from '../../backend/src/models/com_species'
+import { type ref_refAttributes } from '../../backend/src/models/ref_ref'
 
 interface Locality extends now_locAttributes {
   museum_com_mlists: Museum;
   synonyms: string[]
+}
+
+interface Reference extends ref_refAttributes {
 }
 
 interface Species extends com_speciesAttributes {
@@ -13,5 +17,6 @@ interface Species extends com_speciesAttributes {
 export {
   Locality,
   Museum,
+  Reference,
   Species,
 }
