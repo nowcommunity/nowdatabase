ceb9e93cb346412192f7e303fa01c5f15f35f2d8
ShootingStar91 <ttu91@hotmail.com>
2024-05-13 10:10:19 +0300

    hotfix for missing museum router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,6 @@ import localityRouter from './routes/locality'
 import referenceRouter from './routes/reference'
 import speciesRouter from './routes/species'
 import timeUnitRouter from './routes/timeUnit'
-import museumRouter from './routes/museum'
 import { requestLogger, responseLogger } from './middlewares/requestLogger'
 import compression from 'compression'
 import { logger } from './utils/logger'
@@ -31,7 +30,6 @@ app.use('/locality', localityRouter)
 app.use('/reference', referenceRouter)
 app.use('/species', speciesRouter)
 app.use('/time-unit', timeUnitRouter)
-app.use('/museum', museumRouter)
 app.use(errorHandler)
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
