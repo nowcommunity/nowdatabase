2811463aea9a5c0aeb70598707665473549de57c
ShootingStar91 <ttu91@hotmail.com>
2024-05-13 16:22:41 +0300

    Make a common fix big int function

--- backend/src/utils/common.ts
+++ backend/src/utils/common.ts
@@ -1,1 +1,6 @@
 export const sleep = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+export const fixBigInt = (obj: object | null) => obj === null ? null : JSON.stringify(
+  obj,
+  (_key, value) => (typeof value === 'bigint' ? value.toString() : value)
+)
