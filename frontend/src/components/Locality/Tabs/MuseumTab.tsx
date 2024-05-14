1cee6d6261327a4135aca06d0cc0e5025b31bdc7
arttu <ttu91@hotmail.com>
2024-05-14 16:13:04 +0300

    Fix to last commit: remove code

--- frontend/src/components/Locality/Tabs/MuseumTab.tsx
+++ frontend/src/components/Locality/Tabs/MuseumTab.tsx
@@ -1,28 +1,12 @@
 import { Editable, LocalityDetails, Museum } from '@/backendTypes'
-import { EditableTable, EditingModal, Grouped } from '@/components/DetailView/common/FormComponents'
+import { EditableTable, EditingModal, FormTextField, Grouped } from '@/components/DetailView/common/FormComponents'
 import { useDetailContext } from '@/components/DetailView/hooks'
 import { TableView } from '@/components/TableView/TableView'
 import { useGetAllMuseumsQuery } from '@/redux/museumReducer'
-import { Box, CircularProgress, TextField } from '@mui/material'
+import { Box, CircularProgress } from '@mui/material'
 import { MRT_ColumnDef } from 'material-react-table'
 import { useState } from 'react'
-import { FieldErrors, FieldValues, RegisterOptions, UseFormRegisterReturn, useForm } from 'react-hook-form'
-
-const FormTextField = <T extends string>({
-  register,
-  errors,
-  fieldName,
-  label,
-  required = false,
-}: {
-  register: (name: T, options?: RegisterOptions<FieldValues, T> | undefined) => UseFormRegisterReturn<T>
-  errors: FieldErrors<FieldValues>
-  fieldName: T
-  label: string
-  required: boolean
-}) => (
-  <TextField {...register(fieldName, { required: required })} error={!!errors[fieldName]} {...{ label, required }} />
-)
+import { useForm } from 'react-hook-form'
 
 const MuseumSelectingTable = () => {
   const { data, isError } = useGetAllMuseumsQuery()
@@ -130,7 +114,7 @@ export const MuseumTab = () => {
   )
 
   const selectingTable = (
-    <EditingModal buttonText="Add existing museum">
+    <EditingModal buttonText="Add existing museum" onSave={onSave}>
       <MuseumSelectingTable />
     </EditingModal>
   )
