d311f71542ed67b346b78575b7b0088d5cd4d9fa
arttu <ttu91@hotmail.com>
2024-05-22 18:55:21 +0300

    Fix backend building

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,13 +1,23 @@
 import { prisma } from '../utils/db'
 import * as bcrypt from 'bcrypt'
+import { logger } from '../utils/logger'
+import { sleep } from '../utils/common'
 
 export const createTestUser = async () => {
-  const passwordHash = await bcrypt.hash('test', 10)
-
-  await prisma.com_users.create({
-    data: {
-      user_name: 'test',
-      password: passwordHash,
-    },
-  })
+  for (let i = 0; i < 10; i++) {
+    try {
+      const passwordHash = await bcrypt.hash('test', 10)
+      await prisma.com_users.create({
+        data: {
+          user_name: 'test',
+          password: passwordHash,
+        },
+      })
+      logger.info('Test user created.')
+      return
+    } catch {
+      logger.error(`Test user creation failed, attempt ${i} / 10. Trying again in 6 seconds.`)
+      await sleep(6000)
+    }
+  }
 }
