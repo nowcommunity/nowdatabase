651f39efde04485982641e4b85c39346895df285
ShootingStar91 <ttu91@hotmail.com>
2024-05-22 09:21:46 +0300

    Raise payload limit to 50mb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ import { errorHandler } from './middlewares/errorHandler'
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(cors())
 app.use(compression())
 app.use(requestLogger)
