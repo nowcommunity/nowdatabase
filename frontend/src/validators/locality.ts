aba08c450e09aefa2e2f29fde669d6d8369c63e6
arttu <ttu91@hotmail.com>
2024-05-28 22:28:37 +0300

    Fix tsc error

--- frontend/src/validators/locality.ts
+++ frontend/src/validators/locality.ts
@@ -7,6 +7,7 @@ export const validateLocality = (editData: LocalityDetails, fieldName: keyof Loc
       name: 'Basis for age (absolute)',
       asNumber: (num: number) => {
         if (num < parseInt(editData.bfa_min_abs ?? '0')) return 'Max value cannot be lower than min'
+        return
       },
     },
     bfa_min_abs: {
@@ -15,6 +16,7 @@ export const validateLocality = (editData: LocalityDetails, fieldName: keyof Loc
         const bfa_max_abs = editData.bfa_max_abs
         if (!bfa_max_abs) return
         if (parseInt(bfa_max_abs) > num) return 'Min value cannot be higher than max'
+        return
       },
     },
   }
