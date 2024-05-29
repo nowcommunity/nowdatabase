55c7b2593fd8d9026dc09234adcae809144b6fd1
arttu <ttu91@hotmail.com>
2024-05-29 15:15:36 +0300

    Refactor page definitions to their own file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,8 @@ import { Container, Grid } from '@mui/material'
 import { FrontPage } from './components/FrontPage'
 import { NavBar } from './components/NavBar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ReferenceTable } from './components/Reference/ReferenceTable'
-import { SpeciesTable } from './components/Species/SpeciesTable'
-import { TimeUnitTable } from './components/TimeUnit/TimeUnitTable'
 import { Login } from './components/Login'
-import { SpeciesDetails } from './components/Species/SpeciesDetails'
-import { Page } from './components/Page'
-import { LocalityTable } from './components/Locality/LocalityTable'
-import { LocalityDetails } from './components/Locality/LocalityDetails'
-import { LocalityDetailsType, ReferenceDetailsType, SpeciesDetailsType, TimeUnitDetailsType } from './backendTypes'
+import { localityPage, referencePage, speciesPage, timeUnitPage } from './components/pages'
 
 const App = () => {
   return (
@@ -24,54 +17,10 @@ const App = () => {
         <Container maxWidth="xl" fixed style={{ marginTop: '2em', marginBottom: '2em' }}>
           <Grid item>
             <Routes>
-              <Route
-                element={
-                  <Page
-                    tableView={<LocalityTable />}
-                    detailView={<LocalityDetails />}
-                    viewName="locality"
-                    idFieldName="lid"
-                    createTitle={(loc: LocalityDetailsType) => `${loc.loc_name}`}
-                  />
-                }
-                path="/locality/:id?"
-              />
-              <Route
-                element={
-                  <Page
-                    tableView={<SpeciesTable />}
-                    detailView={<SpeciesDetails />}
-                    viewName="species"
-                    idFieldName="species_id"
-                    createTitle={(species: SpeciesDetailsType) => `${species.species_name}`}
-                  />
-                }
-                path="/species/:id?"
-              />
-              <Route
-                element={
-                  <Page
-                    tableView={<ReferenceTable />}
-                    detailView={null}
-                    viewName="reference"
-                    idFieldName="rid"
-                    createTitle={(ref: ReferenceDetailsType) => `${ref.issue}: ${ref.title_primary}`}
-                  />
-                }
-                path="/reference/:id?"
-              />
-              <Route
-                element={
-                  <Page
-                    tableView={<TimeUnitTable />}
-                    detailView={null}
-                    viewName="time-unit"
-                    idFieldName="tu_name"
-                    createTitle={(tu: TimeUnitDetailsType) => `${tu.tu_display_name}`}
-                  />
-                }
-                path="/time-unit/:id?"
-              />
+              <Route element={localityPage} path="/locality/:id?" />
+              <Route element={speciesPage} path="/species/:id?" />
+              <Route element={referencePage} path="/reference/:id?" />
+              <Route element={timeUnitPage} path="/time-unit/:id?" />
               <Route element={<FrontPage />} path="/" />
               <Route element={<Login />} path="/login" />
               <Route element={<div>Page not found.</div>} path="*" />
---
+++ frontend/src/components/pages.tsx
@@ -0,0 +1,48 @@
+import { TimeUnitDetailsType, ReferenceDetailsType, SpeciesDetailsType, LocalityDetailsType } from '@/backendTypes'
+import { LocalityDetails } from './Locality/LocalityDetails'
+import { LocalityTable } from './Locality/LocalityTable'
+import { Page } from './Page'
+import { ReferenceTable } from './Reference/ReferenceTable'
+import { SpeciesDetails } from './Species/SpeciesDetails'
+import { SpeciesTable } from './Species/SpeciesTable'
+import { TimeUnitTable } from './TimeUnit/TimeUnitTable'
+
+export const timeUnitPage = (
+  <Page
+    tableView={<TimeUnitTable />}
+    detailView={null}
+    viewName="time-unit"
+    idFieldName="tu_name"
+    createTitle={(tu: TimeUnitDetailsType) => `${tu.tu_display_name}`}
+  />
+)
+
+export const referencePage = (
+  <Page
+    tableView={<ReferenceTable />}
+    detailView={null}
+    viewName="reference"
+    idFieldName="rid"
+    createTitle={(ref: ReferenceDetailsType) => `${ref.issue}: ${ref.title_primary}`}
+  />
+)
+
+export const speciesPage = (
+  <Page
+    tableView={<SpeciesTable />}
+    detailView={<SpeciesDetails />}
+    viewName="species"
+    idFieldName="species_id"
+    createTitle={(species: SpeciesDetailsType) => `${species.species_name}`}
+  />
+)
+
+export const localityPage = (
+  <Page
+    tableView={<LocalityTable />}
+    detailView={<LocalityDetails />}
+    viewName="locality"
+    idFieldName="lid"
+    createTitle={(loc: LocalityDetailsType) => `${loc.loc_name}`}
+  />
+)
