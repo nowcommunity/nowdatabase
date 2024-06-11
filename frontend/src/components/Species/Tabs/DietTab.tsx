316d651cd58e4a91a1f2b73db9c513ddc767884a
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2024-06-11 13:59:05 +0300

    Edited diet dropdown descriptions

--- frontend/src/components/Species/Tabs/DietTab.tsx
+++ frontend/src/components/Species/Tabs/DietTab.tsx
@@ -5,43 +5,79 @@ import { useDetailContext } from '@/components/DetailView/Context/DetailContext'
 export const DietTab = () => {
   const { dropdown } = useDetailContext<SpeciesDetailsType>()
 
-  const diet1Options = ['', 'animal', 'omnivore', 'plant']
+  const diet1Options = [
+    '',
+    { display: 'Animal', value: 'a' },
+    { display: 'Plant', value: 'p' },
+    { display: 'Omnivore', value: 'o' },
+  ]
 
-  const diet2Options = ['', 'carnivore', 'herbivore', 'invert', 'omnivore', 'piscivore']
+  const diet2Options = [
+    '',
+    { display: 'Carnivore', value: 'carnivore' },
+    { display: 'Herbivore', value: 'herbivore' },
+    { display: 'Invertivore', value: 'invert' },
+    { display: 'Omnivore', value: 'omnivore' },
+    { display: 'Piscivore', value: 'piscivore' },
+  ]
 
   const diet3Options = [
     '',
-    'anim_dom',
-    'br/gr',
-    'browse',
-    'cortex',
-    'exudates',
-    'fr_grass',
-    'fr-se-res',
-    'fruit',
-    'gran',
-    'graze',
-    'h_fruit',
-    'hard',
-    'insect_dom',
-    'm/bone',
-    'meat',
-    'meat_only',
-    'mixed',
-    'myrmec',
-    'plant_dom',
-    'roots',
-    'seeds',
-    'soft',
+    { display: 'Fresh-grass grazers', value: 'fr_grass' },
+    { display: 'Grazer (>90% grass)', value: 'graze' },
+    { display: 'Mixed browser/grazer', value: 'br_gr' },
+    { display: 'Primarily feed on dicot leaves (folivores)', value: 'browse' },
+    { display: 'Bark (tree bark)', value: 'cortex' },
+    { display: 'Exudates: Includes gum, resin, nectar, etc.', value: 'exudates' },
+    { display: 'Combination of fruit, seed, and exudates', value: 'fr-se-res' },
+    { display: 'Fruits (fruit)', value: 'fruit' },
+    { display: 'Fruits (h_fruit)', value: 'h_fruit' },
+    { display: 'Roots', value: 'roots' },
+    { display: 'Granivore (gran)', value: 'gran' },
+    { display: 'Seeds (seeds)', value: 'seeds' },
+    { display: 'Omnivores, plant foods dominant', value: 'plant_dom' },
+    {
+      display:
+        'Omnivores that consume roughly equal proportions of animal and plant foods, with animal food being dominant',
+      value: 'anim_dom',
+    },
+    { display: 'Omnivores, insects dominant', value: 'insect_dom' },
+    { display: 'Meat and specialists on bone consumption (e.g., Hyaena)', value: 'm/bone' },
+    { display: 'Meat predominant, but take other animal, some plant foods', value: 'meat' },
+    { display: 'Meat specialists, do not eat other foods', value: 'meat_only' },
+    { display: 'Molluscivores -- specialists', value: 'hard' },
+    { display: 'Soft and hard invertebrate food', value: 'mixed' },
+    { display: 'Myrmecophages (ant & termite specialists)', value: 'myrmec' },
+    { display: 'Soft invertebrate food', value: 'soft' },
   ]
 
-  const relativeFibreContentOptions = ['', 'h', 'l', 'm']
+  const relativeFibreContentOptions = [
+    '',
+    { display: 'High level of fibre', value: 'h' },
+    { display: 'Medium level of fibre', value: 'm' },
+    { display: 'Low level of fibre', value: 'l' },
+  ]
 
-  const selectivityOptions = ['', 's', 'u']
+  const selectivityOptions = [
+    '',
+    { display: 'Feeds selectively', value: 's' },
+    { display: 'Feeds unselectively', value: 'u' },
+  ]
 
-  const digestionOptions = ['', 'fg', 'hg', 'ru']
+  const digestionOptions = [
+    '',
+    { display: 'Hindgut fermenter', value: 'hg' },
+    { display: 'Foregut fermenter', value: 'fg' },
+    { display: 'True ruminant', value: 'ru' },
+  ]
 
-  const huntForageOptions = ['', 'ambush', 'dig', 'pounce/p', 'pursuit']
+  const huntForageOptions = [
+    '',
+    { display: 'Ambush', value: 'ambush' },
+    { display: 'Dig', value: 'dig' },
+    { display: 'Pounce/Pursuit', value: 'pounce/p' },
+    { display: 'Pursuit', value: 'pursuit' },
+  ]
 
   const diet = [
     ['Diet 1', dropdown('diet1', diet1Options, 'Diet 1')],
@@ -50,7 +86,7 @@ export const DietTab = () => {
     ['Relative Fibre Content', dropdown('rel_fib', relativeFibreContentOptions, 'Relative Fibre Content')],
     ['Selectivity', dropdown('selectivity', selectivityOptions, 'Selectivity')],
     ['Digestion', dropdown('digestion', digestionOptions, 'Digestion')],
-    ['Hunt/Forage', dropdown('hunt_forage', huntForageOptions, 'Hunt/Forage')],
+    ['Hunting or foraging mode for carnivores', dropdown('hunt_forage', huntForageOptions, 'Hunt/Forage')],
   ]
 
   return (
