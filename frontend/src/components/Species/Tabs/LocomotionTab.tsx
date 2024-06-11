0439f12d63a3fc34da88ca1cf24033967caa59b7
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2024-06-11 14:42:58 +0300

    Updated Locomotion display values

--- frontend/src/components/Species/Tabs/LocomotionTab.tsx
+++ frontend/src/components/Species/Tabs/LocomotionTab.tsx
@@ -5,52 +5,91 @@ import { useDetailContext } from '@/components/DetailView/Context/DetailContext'
 export const LocomotionTab = () => {
   const { dropdown } = useDetailContext<SpeciesDetailsType>()
 
-  const feedingHabitat1Options = ['', 'ae', 'aq', 'sa', 'te']
+  const feedingHabitat1Options = [
+    '',
+    { display: 'Aerial', value: 'ae' },
+    { display: 'Aquatic', value: 'aq' },
+    { display: 'Semi-aquatic', value: 'sa' },
+    { display: 'Terrestrial', value: 'te' },
+  ]
 
-  const feedingHabitat2Options = ['', 'aerial', 'aquatic', 'arb/surf', 'canopy', 'caves', 'surface', 'u_ground']
+  const feedingHabitat2Options = [
+    '',
+    { display: 'Feeds primarily while in flight', value: 'aerial' },
+    { display: 'Feeds in the trees, never on ground', value: 'canopy' },
+    { display: 'Feeds on ground and on trunk, lower branches, or even canopy of trees', value: 'arb/surf' },
+    { display: 'Caves', value: 'caves' },
+    { display: 'Feeds only on ground surface', value: 'surface' },
+    { display: 'Feeds primarily underground', value: 'u_ground' },
+    { display: 'Feeds primarily while submerged or at water surface', value: 'aquatic' },
+  ]
 
-  const shelterHabitat1Options = ['', 'ae', 'aq', 'sa', 'te']
+  const shelterHabitat1Options = [
+    '',
+    { display: 'Aerial', value: 'ae' },
+    { display: 'Terrestrial', value: 'te' },
+    { display: 'Semi-aquatic', value: 'sa' },
+    { display: 'Aquatic', value: 'aq' },
+  ]
 
-  const shelterHabitat2Options = ['', 'aquatic', 'arb/surf', 'canopy', 'caves', 'surface', 'u_ground']
-
-  const locomotion1Options = ['', 'ae', 'aq', 'sa', 'te']
+  const shelterHabitat2Options = [
+    '',
+    { display: 'Shelters in the trees, never on ground', value: 'canopy' },
+    { display: 'Shelters on ground or on trunk, lower branches of trees', value: 'arb/surf' },
+    { display: 'Caves', value: 'caves' },
+    { display: 'Shelters only on ground surface', value: 'surface' },
+    { display: 'Shelters primarily underground', value: 'u_ground' },
+    { display: 'Shelters under or at water surface', value: 'aquatic' },
+  ]
+  const locomotion1Options = [
+    '',
+    { display: 'Aerial', value: 'ae' },
+    { display: 'Terrestrial', value: 'te' },
+    { display: 'Semi-aquatic', value: 'sa' },
+    { display: 'Aquatic', value: 'aq' },
+  ]
 
   const locomotion2Options = [
     '',
-    'aerial',
-    'arboreal',
-    'fossorial',
-    'npow_flight',
-    'pow_flight',
-    'scansorial',
-    'semi-fossorial',
-    'surficial',
+    { display: 'Aerial', value: 'aerial' },
+    { display: 'Active flying: most birds, bats', value: 'pow_flight' },
+    { display: 'Gliding, soaring', value: 'npow_flight' },
+    { display: 'Arboreal', value: 'arboreal' },
+    { display: 'Scansorial', value: 'scansorial' },
+    { display: 'Surface', value: 'surficial' },
+    { display: 'Semi-fossorial', value: 'semi-fossorial' },
+    { display: 'Fossorial', value: 'fossorial' },
   ]
 
   const locomotion3Options = [
     '',
-    'arb_above_br',
-    'arb_suspensory',
-    'bipedal_str',
-    'cursorial',
-    'fast_flight',
-    'gen_quad',
-    'glide',
-    'graviportal',
-    'hover',
-    'k_walk',
-    'parachute',
-    'saltatory',
-    's/f_flight',
-    'slow_flight',
-    'soar',
-    'undulate',
-    'sub_curs',
-    'hyper_curs',
-    'swim_parax',
+    { display: 'Arboreal, above branch', value: 'arb_above_br' },
+    { display: 'Arboreal, below-branch suspensory', value: 'arb_suspensory' },
+    { display: 'Bipedal striding', value: 'bipedal_str' },
+    { display: 'Running specialists', value: 'cursorial' },
+    { display: 'Fast powered flight (e.g., ducks, free-tailed bats)', value: 'fast_flight' },
+    { display: 'General, unspecialized quadrupedal', value: 'gen_quad' },
+    { display: 'Short, controlled nonpowered flight, glide angle < 45deg', value: 'glide' },
+    { display: 'Graviportal, e.g., elephants', value: 'graviportal' },
+    { display: 'Powered flight  with specializations for hovering', value: 'hover' },
+    { display: 'Knuckle-walking', value: 'k_walk' },
+    { display: 'Nonpowered flight, glide angle >45deg', value: 'parachute' },
+    { display: 'Jumping, hopping, "ricochetal"', value: 'saltatory' },
+    { display: 'Fly slow or fast', value: 's/f_flight' },
+    { display: 'Slow powered flight', value: 'slow_flight' },
+    { display: 'All types of soaring', value: 'soar' },
+    { display: 'Undulate, e.g., snakes', value: 'undulate' },
+    { display: 'Sub-cursorial', value: 'sub_curs' },
+    { display: 'Hyper-cursorial', value: 'hyper_curs' },
+    { display: 'Swim-paraxial', value: 'swim_parax' },
   ]
 
-  const activityOptions = ['', 'crepuscular', 'diurnal', 'nocturnal']
+  const activityOptions = [
+    '',
+    { display: 'Diurnal', value: 'd' },
+    { display: 'Crepuscular', value: 'c' },
+    { display: 'Nocturnal', value: 'n' },
+  ]
 
   const feedingHabitat = [
     ['Feeding Habitat 1', dropdown('feedinghab1', feedingHabitat1Options, 'Feeding Habitat 1')],
