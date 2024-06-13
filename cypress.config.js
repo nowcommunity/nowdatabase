ce09f63ab0fa696cfc7c5df44dcd0e3e2b18e811
ShootingStar91 <ttu91@hotmail.com>
2024-06-13 17:09:42 +0300

    Increase cypress timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 12000,
+    execTimeout: 90000,
+    pageLoadTimeout: 140000,
+    requestTimeout: 9000,
+    responseTimeout: 45000,
   },
 });
