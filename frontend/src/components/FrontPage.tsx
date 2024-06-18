4075294e81e2da936daa9e7282e8e0e550d95a2b
arttu <ttu91@hotmail.com>
2024-06-18 23:06:58 +0300

    Add links and reminder of test data on front page

--- frontend/src/components/FrontPage.tsx
+++ frontend/src/components/FrontPage.tsx
@@ -5,6 +5,17 @@ export const FrontPage = () => {
     <Box>
       <h1>Nowdatabase</h1>
       <p>Site under construction.</p>
+      <p>
+        <b>This version is running on test data. May contain incorrect and outdated information.</b>
+      </p>
+      <p>
+        Project repository:{' '}
+        <a href="https://github.com/nowcommunity/nowdatabase">https://github.com/nowcommunity/nowdatabase</a>
+      </p>
+      <p>
+        Have any questions or suggestions for improvement? Make an issue by logging into GitHub and clicking new issue
+        on this page: <a href="https://github.com/nowcommunity/nowdatabase">GitHub Issues</a>
+      </p>
     </Box>
   )
 }
