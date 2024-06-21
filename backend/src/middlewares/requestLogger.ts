1c90b6ea3943fe41dc1af84fb0075c24aad1bf62
arttu <ttu91@hotmail.com>
2024-06-21 13:35:27 +0300

    Add res.send to response logger

--- backend/src/middlewares/requestLogger.ts
+++ backend/src/middlewares/requestLogger.ts
@@ -10,12 +10,16 @@ export const requestLogger: Middleware = (req, _res, next) => {
 
 export const responseLogger: Middleware = (req, res, next) => {
   const originalJson = res.json
+  const originalSend = res.send
   const getLogString = (req: express.Request) =>
     `${res.statusCode} ${req.method} ${req.baseUrl}${req.url} ${req.user ? `Username: '${req.user.username}'` : ''}`
   res.json = function (body) {
     logger.info(getLogString(req))
     return originalJson.call(this, body)
   }
-
+  res.send = function (body) {
+    logger.info(getLogString(req))
+    return originalSend.call(this, body)
+  }
   next()
 }
