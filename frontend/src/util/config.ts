daafb3069e102861002fc2f3107bd9f835590d9a
ShootingStar91 <ttu91@hotmail.com>
2024-08-02 14:41:32 +0300

    Handle build env vars via dockerfile args

--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL as string | undefined) ?? 'http://localhost:4000/'
 export const ENV = import.meta.env.VITE_RUNNING_ENV as 'dev' | 'staging' | 'prod'
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string
 export const ENABLE_WRITE = import.meta.env.VITE_ENABLE_WRITE === 'true'
