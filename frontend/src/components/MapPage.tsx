616b8a206dce3bfee0ef34507fec7facb3c96c06
Miina Saroma <miina.saroma@helsinki.fi>
2024-09-10 15:12:46 +0300

    Add plain map

--- frontend/src/components/MapPage.tsx
+++ frontend/src/components/MapPage.tsx
@@ -1,9 +1,19 @@
-import { Container } from '@mui/material'
+import { useRef } from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export const MapPage = () => {
+  const mapRef = useRef(null)
+  const latitude = 30
+  const longitude = -5
+
   return (
-    <Container style={{ alignContent: 'center' }} maxWidth="sm">
-      <p style={{ fontSize: 20 }}>Add map somewhere here</p>
-    </Container>
+    <MapContainer center={[latitude, longitude]} zoom={2} ref={mapRef} style={{ height: '50vh', width: '50vw' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* Additional map layers or components can be added here */}
+    </MapContainer>
   )
 }
