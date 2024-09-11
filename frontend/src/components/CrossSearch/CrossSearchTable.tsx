7a880a40bab991498dfa26d29d5d5bb8c72b3a36
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-09-11 15:31:27 +0300

    mvp online

--- frontend/src/components/CrossSearch/CrossSearchTable.tsx
+++ frontend/src/components/CrossSearch/CrossSearchTable.tsx
@@ -35,6 +35,51 @@ export const CrossSearchTable = ({ selectorFn }: { selectorFn?: (newObject: Cros
         header: 'Min age',
         filterVariant: 'range',
       },
+      {
+        accessorKey: 'subclass_or_superorder_name',
+        header: 'Subclass or Superorder',
+        size: 20,
+      },
+      {
+        accessorKey: 'order_name',
+        header: 'Order',
+        size: 20,
+      },
+      {
+        accessorKey: 'suborder_or_superfamily_name',
+        header: 'Suborder or Superfamily',
+        size: 20,
+      },
+      {
+        accessorKey: 'family_name',
+        header: 'Family',
+        size: 20,
+      },
+      {
+        accessorKey: 'subfamily_name',
+        header: 'Subfamily or Tribe',
+        size: 20,
+      },
+      {
+        accessorKey: 'genus_name',
+        header: 'Genus',
+        size: 20,
+      },
+      {
+        accessorKey: 'species_name',
+        header: 'Species',
+        size: 20,
+      },
+      {
+        accessorKey: 'unique_identifier',
+        header: 'Unique Identifier',
+        size: 20,
+      },
+      {
+        accessorKey: 'taxonomic_status',
+        header: 'Taxonomic Status',
+        size: 20,
+      },
     ],
     []
   )
