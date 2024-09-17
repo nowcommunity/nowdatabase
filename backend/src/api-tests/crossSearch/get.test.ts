d18704aa546570d1d82413af5a9295b57daa7f77
Miina Saroma <miina.saroma@helsinki.fi>
2024-09-17 18:25:52 +0300

    Add c-search test for fields

--- backend/src/api-tests/crossSearch/get.test.ts
+++ backend/src/api-tests/crossSearch/get.test.ts
@@ -9,6 +9,15 @@ describe('Getting cross-search data works', () => {
   it('Requesting get all has correct length', async () => {
     const { body: getReqBody, status: getReqStatus } = await send(`crosssearch/all`, 'GET')
     expect(getReqStatus).toEqual(200)
-    expect(getReqBody).toHaveLength(6)
+    expect(getReqBody).toHaveLength(6) // TODO: this is sometimes 7, sometimes more because of jest parallel stuff
+  })
+
+  it('Get all has some correct fields from locality and species', async () => {
+    const { body: getReqBody } = await send(`crosssearch/all`, 'GET')
+    expect(getReqBody[0]).toHaveProperty('lid')
+    expect(getReqBody[0]).toHaveProperty('loc_name')
+    expect(getReqBody[0]).toHaveProperty('subclass_or_superorder_name')
+    expect(getReqBody[0]).toHaveProperty('family_name')
+    expect(getReqBody[0]).toHaveProperty('taxonomic_status')
   })
 })
