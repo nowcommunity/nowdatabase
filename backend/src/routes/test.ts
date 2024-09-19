8f35af289c2b894f963195a6fb3474452b95da15
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-09-19 16:27:10 +0200

    fixed the healthcheck to actually work

--- backend/src/routes/test.ts
+++ backend/src/routes/test.ts
@@ -7,4 +7,8 @@ router.get('/create-test-users', async (_req, res) => {
   res.status(200).send()
 })
 
+router.get("/ping", async (_req, res) => {
+    return res.status(200).send()
+})
+
 export default router
