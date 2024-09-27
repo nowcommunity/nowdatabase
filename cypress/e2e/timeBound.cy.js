84bf3b9460f7d798b0537333c67b47c4e1161433
juhanikat <juhani.kataja@gmail.com>
2024-09-27 13:42:32 +0300

    add database reset

--- cypress/e2e/timeBound.cy.js
+++ cypress/e2e/timeBound.cy.js
@@ -2,6 +2,9 @@ describe("Editing time bound", () => {
     beforeEach('Login as admin', () => {
         cy.login('testSu')
     })
+    beforeEach('Reset database', () => {
+        cy.request(Cypress.env("databaseResetUrl"));
+    });
     it("User editing time bound with correct values succeeds", () => {
         cy.visit(`/time-bound/11?tab=0`)
         cy.contains('C2N-y')
@@ -12,14 +15,6 @@ describe("Editing time bound", () => {
         cy.get('[id=write-button]').click()
         cy.get('[id=write-button]').click()
         cy.contains("2.5")
-
-        cy.get('[id=edit-button]').click()
-        // this id might change which breaks the test, don't know why
-        cy.get("[id=\\:r5\\:]").first().type("{backspace}{backspace}{backspace}1.778")
-        cy.contains("This field is required").should('not.exist')
-        cy.get('[id=write-button]').click()
-        cy.get('[id=write-button]').click()
-        cy.contains("1.778")
     })
     it("User editing time bound with incorrect values is notified about it", () => {
         cy.visit(`/time-bound/11?tab=0`)
