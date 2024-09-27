fb2e079a5ed996142d60b58244d376bd6ee838ed
juhanikat <juhani.kataja@gmail.com>
2024-09-27 10:23:30 +0300

    second test, still wip

--- backend/src/api-tests/timeBound/update.test.ts
+++ backend/src/api-tests/timeBound/update.test.ts
@@ -1,7 +1,6 @@
 import { describe, it, beforeAll, beforeEach, expect } from '@jest/globals'
 import { editedTimeBound } from './data'
 import { send, resetDatabase, login } from '../utils'
-import { TimeBoundDetailsType } from '../../../../frontend/src/backendTypes'
 
 describe('Time bound updating', () => {
   beforeAll(async () => {
@@ -10,7 +9,15 @@ describe('Time bound updating', () => {
   beforeEach(async () => {
     await login()
   })
-  it('Update request succeeds and returns valid number id', async () => {
+  it('Update request with valid timebound succeeds and returns valid number id', async () => {
+    const { body: resultBody } = await send<{ id: number }>('time-bound', 'PUT', {
+      timeBound: { ...editedTimeBound },
+    })
+    const { id: existingId } = resultBody
+
+    expect(typeof existingId).toEqual('number') // `Invalid result returned on write: ${createdId}`
+  })
+  it('Update request with invalid timebound age fails', async () => {
     const { body: resultBody } = await send<{ id: number }>('time-bound', 'PUT', {
       timeBound: { ...editedTimeBound },
     })
