878b5ee0334fcc54b86c90e6f1dd05aadc9df890
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-09-28 21:46:30 +0200

    added coverage reporting for frontend

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
+    setupNodeEvents(on, _config) {
+      require('@cypress/code-coverage/task')(on, _config)
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return _config
     },
     baseUrl: 'http://localhost:5173/',
   },
   env: {
-    databaseResetUrl: "http://backend:4000/test/reset-test-database"
+    databaseResetUrl: "http://localhost:4000/test/reset-test-database"
   },
   viewportHeight: 900,
   viewportWidth: 1200,
