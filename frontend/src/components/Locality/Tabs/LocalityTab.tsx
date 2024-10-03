f5b7e9ae131232d348418dccbc48dc04551a6366
Miina Saroma <miina.saroma@helsinki.fi>
2024-10-03 15:52:07 +0300

    Location coordinates are copied to dec input fields

--- frontend/src/components/Locality/Tabs/LocalityTab.tsx
+++ frontend/src/components/Locality/Tabs/LocalityTab.tsx
@@ -7,9 +7,13 @@ import { useForm } from 'react-hook-form'
 import { EditableTable } from '@/components/DetailView/common/EditableTable'
 import { EditingModal } from '@/components/DetailView/common/EditingModal'
 import { emptyOption } from '@/components/DetailView/common/misc'
+import { Map } from '@/components/Map/Map'
+import { useState } from 'react'
 
 export const LocalityTab = () => {
   const { textField, radioSelection, dropdown, mode, bigTextField } = useDetailContext<LocalityDetailsType>()
+  const { editData, setEditData } = useDetailContext<LocalityDetailsType>()
+  const [coordinates, setCoordinates] = useState(null)
 
   const approximateCoordinatesOptions = [
     { display: 'No', value: 'false' },
@@ -83,6 +87,22 @@ export const LocalityTab = () => {
     </EditingModal>
   )
 
+  // eslint-disable-next-line @typescript-eslint/require-await
+  const onSaveCoord = async () => {
+    console.log('Coordinates:', coordinates.lat, coordinates.lng) //this is just dec
+    setEditData({ ...editData, dec_lat: coordinates.lat, dec_long: coordinates.lng })
+    return Object.keys(errors).length === 0 //no idea if this is needed, just copypasted
+  }
+
+  // TODO name this better, plagiarized from editingModal
+  const coordinateButton = (
+    <EditingModal buttonText="Get Coordinates" onSave={onSaveCoord}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
+        <Map coordinates={coordinates} setCoordinates={setCoordinates} />
+      </Box>
+    </EditingModal>
+  )
+
   return (
     <>
       <HalfFrames>
@@ -90,7 +110,10 @@ export const LocalityTab = () => {
         <ArrayFrame half array={country} title="Country" />
       </HalfFrames>
 
-      <ArrayFrame array={latlong} title="Latitude & Longitude" />
+      <Grouped>
+        <ArrayFrame array={latlong} title="Latitude & Longitude" />
+        {!mode.read && coordinateButton}
+      </Grouped>
 
       <Grouped title="Synonyms">
         {!mode.read && editingModal}
