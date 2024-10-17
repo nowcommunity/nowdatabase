6a7b9a2bf87f9eb22436dd43efc802f5ef5b72ec
juhanikat <juhani.kataja@gmail.com>
2024-10-17 13:09:52 +0300

    test that logs are added properly when editing time bound

--- backend/src/api-tests/timeBound/update.test.ts
+++ backend/src/api-tests/timeBound/update.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, beforeAll, beforeEach, expect } from '@jest/globals'
 import { editedTimeBound, newTimeBoundBasis } from './data'
-import { send, resetDatabase, login, resetDatabaseTimeout } from '../utils'
+import { send, resetDatabase, login, resetDatabaseTimeout, testLogRows } from '../utils'
 import { TimeBoundDetailsType } from '../../../../frontend/src/backendTypes'
+import { LogRow } from '../../services/write/writeOperations/types'
 
 const existingTimeBound = { ...newTimeBoundBasis, bid: 11 }
 
@@ -52,5 +53,26 @@ describe('Time bound updating', () => {
     expect(body.bid).toEqual(existingTimeBound.bid)
     expect(body.b_name).toEqual(existingTimeBound.b_name)
     expect(body.age).toEqual(existingTimeBound.age)
+  })
+  it('Update logs are correct', async () => {
+    // edit time unit to create a log update
+    const { body: resultBody } = await send<{ bid: number }>('time-bound', 'PUT', {
+      timeBound: editedTimeBound,
+    })
+    const { bid: createdId } = resultBody
+    const { body: updatedTimeBound } = await send<TimeBoundDetailsType>(`time-bound/${createdId}`, 'GET')
+    const lastUpdate = updatedTimeBound.now_bau[updatedTimeBound.now_bau.length - 1]
+    expect(lastUpdate.bau_comment).toEqual(editedTimeBound.comment) // 'Comment is correct'
+    const logRows = lastUpdate.updates
+    const expectedLogRows: Partial<LogRow>[] = [
+      {
+        table: 'now_tu_bound',
+        column: 'b_name',
+        oldValue: newTimeBoundBasis.b_name,
+        value: editedTimeBound.b_name,
+        type: 'update',
+      },
+    ]
+    testLogRows(logRows, expectedLogRows, 2)
   })
 })
