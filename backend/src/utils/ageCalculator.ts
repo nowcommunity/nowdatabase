5c8d03085cdc2b9e0c3197b1d2b645d90d758ceb
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-10-23 12:07:11 +0300

    added check for falsy fractions rather than null

--- backend/src/utils/ageCalculator.ts
+++ backend/src/utils/ageCalculator.ts
@@ -1,5 +1,5 @@
 export const calculateLocalityMaxAge = (upperBound: number, lowerBound: number, frac: string | null) => {
-  if (frac === null) {
+  if (!frac) {
     return lowerBound
   }
   const parts = frac.split(':')
@@ -7,15 +7,15 @@ export const calculateLocalityMaxAge = (upperBound: number, lowerBound: number, 
   const numerator = parseInt(parts[0])
   const denominator = parseInt(parts[1])
 
-  if (denominator === 0) {
+  if (denominator === 0) {     // This shouldn't be possible
     throw new Error('Denominator cannot be 0')
   }
-
+  
   return upperBound + ((lowerBound - upperBound) * numerator) / denominator
 }
 
 export const calculateLocalityMinAge = (upperBound: number, lowerBound: number, frac: string | null) => {
-  if (frac === null) {
+  if (!frac) {
     return upperBound
   }
   const parts = frac.split(':')
@@ -23,7 +23,7 @@ export const calculateLocalityMinAge = (upperBound: number, lowerBound: number, 
   const numerator = parseInt(parts[0])
   const denominator = parseInt(parts[1])
 
-  if (denominator === 0) {
+  if (denominator === 0) {      // This shouldn't be possible
     throw new Error('Denominator cannot be 0')
   }
 
