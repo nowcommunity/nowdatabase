778f4620657eb3d9afe7ef6318da9ac1fc8e093c
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-10-25 11:44:52 +0200

    implemented few tests for user route + added few todo tests

---
+++ backend/src/api-tests/user/create.test.ts
@@ -0,0 +1,16 @@
+import { beforeAll, afterAll, describe, it } from '@jest/globals'
+import { resetDatabase, resetDatabaseTimeout } from '../utils'
+import { pool } from '../../utils/db'
+
+describe('Creating an user', () => {
+  beforeAll(async () => {
+    await resetDatabase()
+  }, resetDatabaseTimeout)
+  afterAll(async () => {
+    await pool.end()
+  })
+
+  it.todo("is successful with correct data")
+  it.todo("is not successfull with missing data")
+  it.todo("is not successfull with incorrect password")
+})
---
+++ backend/src/api-tests/user/data.ts
@@ -0,0 +1,17 @@
+export const testSu = {
+  username: "testSu",
+  password: "test"
+}
+
+export const newUser = {
+  username: "charliechaplin123",
+  password: "mutefilms",
+  initials: "CC",
+  role: "su",
+  secret: "abcdefg"
+}
+
+export default {
+  testSu,
+  newUser
+}
