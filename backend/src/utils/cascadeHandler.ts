2a21c34c5ccf6cb9ae9f91fc5ed28653a5b12302
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-11-01 14:43:22 +0200

    initiate a function to handle locality updates for multple affected timeunits

--- backend/src/utils/cascadeHandler.ts
+++ backend/src/utils/cascadeHandler.ts
@@ -56,14 +56,20 @@ type Bounds = {
 }
 
 type Unit = {
+  tu_name: string
   up_bnd: number
   low_bnd: number
 }
 
 
 export const checkTimeBoundCascade = async (timeBound: EditDataType<TimeBoundDetailsType>) => {
+  const cascadeErrors: string[] = []
+  const calculatorErrors: string[] = []
+  const localitiesToCheck = []
+  const timeUnitsToUpdate = []
   const timeUnits = await nowDb.now_time_unit.findMany({
     select: {
+      tu_name: true,
       up_bnd: true,
       low_bnd: true,
     },
@@ -82,28 +88,64 @@ export const checkTimeBoundCascade = async (timeBound: EditDataType<TimeBoundDet
       },
     })
     if (bounds.length === 2) {
-      console.log(checkAges(timeUnit, bounds as Bounds[], timeBound.age as number, timeBound.bid as number))
+      const editedBounds = calculateTimeUnitAges(timeUnit, bounds as Bounds[], timeBound.age as number, timeBound.bid as number);
+      if (editedBounds) {
+        if (editedBounds.lowBoundAge > editedBounds.upBoundAge) {
+          timeUnitsToUpdate.push(timeUnit)
+        } else {
+          cascadeErrors.push(timeUnit.tu_name)
+        }
+      } else {
+        calculatorErrors.push(timeUnit.tu_name)
+      }
     } else {
       console.log("Not enough bounds found for time unit. Bad")
     }
   }
-  return { cascadeErrors: [], calculatorErrors: [], timeUnitsToUpdate: [] }
+  for (const timeUnit of timeUnitsToUpdate) {
+    const localities = await nowDb.now_loc.findMany({
+      select: {
+        loc_name: true,
+        max_age: true,
+        min_age: true,
+        bfa_max: true,
+        bfa_min: true,
+        frac_max: true,
+      },
+      where: {
+        OR: [{ bfa_max: timeUnit.tu_name }, { bfa_min: timeUnit.tu_name }],
+      },
+    })
+    for (const locality of localities) {
+      localitiesToCheck.push(locality)
+    }
+  }
+  const { cascadeErrorsLocalities, calculatorErrorsLocalities, localitiesToUpdate } = await handleLocalityAndTimeUnitLists(localitiesToCheck, timeUnitsToUpdate)
+  console.log(localitiesToCheck)
+  return { cascadeErrors, calculatorErrors, localitiesToUpdate, timeUnitsToUpdate }
 }
 
-function checkAges(
+const calculateTimeUnitAges = (
   unit: Unit,
   list: Bounds[],
   editedAge: number,
   bid: number
-): boolean | undefined {
+) => {
   const upBoundEntry = list.find(item => item.bid === unit.up_bnd);
   const lowBoundEntry = list.find(item => item.bid === unit.low_bnd);
   if (upBoundEntry && lowBoundEntry) {
     const upBoundAge = unit.up_bnd === bid ? editedAge : upBoundEntry.age;
     const lowBoundAge = unit.low_bnd === bid ? editedAge : lowBoundEntry.age;
-    return upBoundAge < lowBoundAge;
+    return { upBoundAge, lowBoundAge };
   } else {
     console.error("One or both bounds not found in the list.");
     return undefined;
   }
+}
+
+const handleLocalityAndTimeUnitLists = (localities: any[], timeUnits: any[]) => {
+  const cascadeErrorsLocalities: string[] = []
+  const calculatorErrorsLocalities: string[] = []
+  const localitiesToUpdate: any[] = []
+  return { cascadeErrorsLocalities, calculatorErrorsLocalities, localitiesToUpdate }
 }
