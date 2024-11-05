8d889b7b443c9f8a38a7010a0bb44e5e626016ff
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-11-05 15:11:46 +0200

    enable service to fetch data for serverside filtering
    Co-authored-by: Aaro Koivukangas<aaro.koivukangas@gmail.com>

--- backend/src/routes/crossSearch.ts
+++ backend/src/routes/crossSearch.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
-import { getAllCrossSearch } from '../services/crossSearch'
+import { getAllCrossSearch, getFilteredCrossSearch } from '../services/crossSearch'
 import { fixBigInt } from '../utils/common'
+import { ColumnFilter, Sorting, Page } from '../../../frontend/src/backendTypes'
+import { paginateList } from '../utils/pagination'
 
 const router = Router()
 
@@ -11,10 +13,17 @@ router.get('/all/*', async (req, res) => {
 })
 
 router.get(`/`, async (req, res) => {
-  const columnfilter = req.query.columnfilters;
-  const sorting = req.query.sorting;
-  const page = req.query.pagination;
-  return res.status(200).send([]);
+  const columnfilter = req.query.columnfilters as ColumnFilter[] | []
+  console.log('columnfilter', columnfilter)
+  const sorting = req.query.sorting as Sorting[] | []
+  console.log('sorting', sorting)
+  const data = await getFilteredCrossSearch(columnfilter, sorting, req.user)
+  console.log('data', data)
+  const page = req.query.pagination as unknown as Page
+  if (!page) return res.status(200).send([])  // there is no pagination in the request when navigating to the page at first
+  console.log('getting filtered cross search')
+  const paginatedData = paginateList(data, page)
+  return res.status(200).send(paginatedData)
 });
 
 export default router
