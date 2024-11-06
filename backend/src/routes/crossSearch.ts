1ccfc4dd803ac875334f5248ac7f5b0419db3336
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-11-06 14:41:46 +0200

    first data from serverside filtering to front

--- backend/src/routes/crossSearch.ts
+++ backend/src/routes/crossSearch.ts
@@ -14,16 +14,12 @@ router.get('/all/*', async (req, res) => {
 
 router.get(`/`, async (req, res) => {
   const columnfilter = req.query.columnfilters as ColumnFilter[] | []
-  console.log('columnfilter', columnfilter)
   const sorting = req.query.sorting as Sorting[] | []
-  console.log('sorting', sorting)
-  const data = await getFilteredCrossSearch(columnfilter, sorting, req.user)
-  console.log('data', data)
-  const page = req.query.pagination as unknown as Page
+  const page = req.query.pagination as unknown as string
   if (!page) return res.status(200).send([])  // there is no pagination in the request when navigating to the page at first
-  console.log('getting filtered cross search')
-  const paginatedData = paginateList(data, page)
-  return res.status(200).send(paginatedData)
+  const pageObject = JSON.parse(page as string) as Page
+  const data = await getFilteredCrossSearch(columnfilter, sorting, pageObject, req.user)
+  return res.status(200).send(fixBigInt(data))
 });
 
 export default router
