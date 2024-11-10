026fc3883a4bdef45448d2e367ea0ad508a0e6dc
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2024-11-10 23:27:26 +0200

    remove accidental import

--- frontend/src/components/Locality/Tabs/AgeTab.tsx
+++ frontend/src/components/Locality/Tabs/AgeTab.tsx
@@ -1,4 +1,4 @@
-import { EditDataType, LocalityDetailsType, TimeUnitDetailsType } from '@/backendTypes'
+import { LocalityDetailsType, TimeUnitDetailsType } from '@/backendTypes'
 import { BasisForAgeSelection } from '@/components/DetailView/common/editingComponents'
 import { emptyOption } from '@/components/DetailView/common/misc'
 import { ArrayFrame, HalfFrames } from '@/components/DetailView/common/tabLayoutHelpers'
