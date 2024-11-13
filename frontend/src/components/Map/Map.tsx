5a2f55c1284bb24ebde5103ba58284d5c6114f75
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2024-11-13 07:26:57 +0200

    implement location search

--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,5 +1,6 @@
-import { useRef, useMemo } from 'react'
+import { useRef, useMemo, useState, KeyboardEvent } from 'react'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { TextField, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import 'leaflet/dist/leaflet.css'
 
 type Coordinate = {
@@ -26,6 +27,88 @@ const DraggableMarker = ({ setMarkerPos, coord }: { setMarkerPos: CoordinateSett
   return <Marker draggable={true} eventHandlers={eventHandlers} position={coord} ref={markerRef} />
 }
 
+const LocationSearch = () => {
+  type Location = {
+    name: string
+    country: string
+    latitude: number
+    longitude: number
+  }
+
+  const [searchBoxValue, setSearchBoxValue] = useState('')
+  const [resultsList, setResultsList] = useState<Location[]>([])
+
+  const handleSearch = async () => {
+    const username = ''
+    const url = `http://api.geonames.org/searchJSON?q=${searchBoxValue}&maxRows=5&username=${username}`
+
+    type Geoname = {
+      adminCode01: string
+      lng: string
+      geonameId: number
+      toponymName: string
+      countryId: string
+      fcl: string
+      population: number
+      countryCode: string
+      name: string
+      fclName: string
+      adminCodes1: {
+        ISO3166_2: string
+      }
+      countryName: string
+      fcodeName: string
+      adminName1: string
+      lat: string
+      fcode: string
+    }
+    type GeoNamesJSON = {
+      totalResultsCount: number
+      geonames: Geoname[]
+    }
+
+    try {
+      const result = (await (await fetch(url)).json()) as GeoNamesJSON
+      const locations = result.geonames.map(geoname => ({
+        name: geoname.name,
+        country: geoname.countryName,
+        latitude: Number(geoname.lat),
+        longitude: Number(geoname.lng),
+      }))
+      setResultsList(locations)
+    } catch (error) {
+      throw new Error('Location search failed')
+    }
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      void handleSearch()
+    }
+  }
+
+  return (
+    <div>
+      <TextField
+        onChange={event => {
+          setSearchBoxValue(event.target.value)
+        }}
+        value={searchBoxValue}
+        onKeyDown={handleKeyDown}
+      />
+      <List>
+        {resultsList.map((result, index) => (
+          <ListItem key={index} sx={{ backgroundColor: 'lightgray' }}>
+            <ListItemButton>
+              <ListItemText primary={`${result.name}, ${result.country}`} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+}
+
 export const Map = ({ coordinates, setCoordinates }: { coordinates: Coordinate; setCoordinates: CoordinateSetter }) => {
   const mapRef = useRef(null)
   const latitude = 30
@@ -44,6 +127,7 @@ export const Map = ({ coordinates, setCoordinates }: { coordinates: Coordinate; 
       <p>
         Lat: {coordinates ? coordinates.lat : null} Lon: {coordinates ? coordinates.lng : null}
       </p>
+      <LocationSearch />
     </div>
   )
 }
