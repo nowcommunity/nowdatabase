38fdecad68a1ed9a7c03b69196c092e598b96b20
Eero Antikainen <eero.antikainen@helsinki.fi>
2024-11-15 20:36:02 +0200

    removed unused draft

--- backend/src/utils/sql.ts
+++ backend/src/utils/sql.ts
@@ -100,32 +100,3 @@ export const generateFilteredCrossSearchSqlWithAdmin = (limit: number, offset: n
     ${offset}
   `
 }
-
-export const oldGenerateFilteredCrossSearchSql = (limit: number) => {
-  return Prisma.sql`
-  SELECT 
-    now_plr.pid,
-    now_loc.lid,
-    loc_name, 
-    country,
-    com_species.species_id
-  FROM 
-   now_ls 
-  LEFT JOIN
-   now_loc
-  ON
-   now_ls.lid = now_loc.lid 
-  LEFT JOIN
-   com_species
-  ON
-   now_ls.species_id = com_species.species_id
-  LEFT JOIN
-    now_plr
-  ON
-    now_loc.lid = now_plr.lid  
-  ORDER BY
-    now_loc.lid
-  LIMIT
-   ${limit}
-  `
-}
