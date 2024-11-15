bce15e5cf5fc7a26e1db54b0618c36cfa6cfb531
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-11-15 13:25:27 +0100

    continued work on raw sql query

--- backend/src/routes/crossSearch.ts
+++ backend/src/routes/crossSearch.ts
@@ -1,38 +1,73 @@
 import { Router } from 'express'
-import { getAllCrossSearch, getFilteredCrossSearch, getFilteredCrossSearchLength, getTestQuery } from '../services/crossSearch'
+import {
+  getAllCrossSearch,
+  getFilteredCrossSearch,
+  getFilteredCrossSearchLength,
+  getTestQuery,
+  getFilteredCrossSearchRawSql,
+} from '../services/crossSearch'
 import { fixBigInt } from '../utils/common'
+import { isPage } from '../utils/url'
 import { ColumnFilter, Sorting, Page } from '../../../frontend/src/backendTypes'
 
 const router = Router()
 
-router.get('/all/*', async (req, res) => {
+router.get('/all', async (req, res) => {
   console.log('getting all cross search')
+  console.time('Cross search all')
   const crossSearch = await getAllCrossSearch(req.user)
+  console.timeEnd('Cross search all')
   return res.status(200).send(fixBigInt(crossSearch))
 })
 
-router.get('/testing', async (req, res) => {
+router.get('/testing', async (_req, res) => {
   console.log('getting all cross search')
   await getTestQuery()
   return res.status(200).send()
+})
+
+router.get('/testing/all', async (req, res) => {
+  const columnfilter = req.query.columnfilters
+  const sorting = req.query.sorting
+  const page = req.query.pagination
+
+  console.log('page:', page, !isPage(page))
+  if (!isPage(page)) return res.status(200).send([]) // there is no pagination in the request when navigating to the page at first
+
+  console.log('page is')
+
+  // TODO remove "as string"
+  const columnfilterObject = JSON.parse(columnfilter as string) as ColumnFilter[]
+  const sortingObject = JSON.parse(sorting as string) as Sorting[]
+  const pageObject = JSON.parse(page as string) as Page
+
+  console.log('pageObject:', pageObject)
+
+  const result = (await getFilteredCrossSearchRawSql(columnfilterObject, sortingObject, pageObject, req.user)) as any
+
+  return res.status(200).send({ result })
 })
 
 router.get(`/`, async (req, res) => {
   const columnfilter = req.query.columnfilters as unknown as string
   const sorting = req.query.sorting as unknown as string
   const page = req.query.pagination as unknown as string
-  if (!page) return res.status(200).send([])  // there is no pagination in the request when navigating to the page at first
+
+  if (!page) return res.status(200).send([]) // there is no pagination in the request when navigating to the page at first
+
   const columnfilterObject = JSON.parse(columnfilter as string) as ColumnFilter[]
   const sortingObject = JSON.parse(sorting as string) as Sorting[]
-  console.log('sortingObject',sortingObject)
+  console.log('sortingObject', sortingObject)
   const pageObject = JSON.parse(page as string) as Page
-  const data = await getFilteredCrossSearch(columnfilterObject, sortingObject, pageObject, req.user) as any
+
+  const data = (await getFilteredCrossSearch(columnfilterObject, sortingObject, pageObject, req.user)) as any
   const rowCount = await getFilteredCrossSearchLength(columnfilterObject, req.user)
+
   const result = {
     data: fixBigInt(data),
     rowCount,
   }
   return res.status(200).send(result)
-});
+})
 
 export default router
