0ad0dc869f43b2a855d86b62a1153913afd10fcf
juhanikat <juhani.kataja@gmail.com>
2025-01-18 14:54:00 +0200

    add functionality for adding/editing regions

--- backend/src/services/write/region.ts
+++ backend/src/services/write/region.ts
@@ -1,44 +1,22 @@
-import { EditDataType, Reference, User, RegionDetails } from '../../../../frontend/src/shared/types'
-import { NOW_DB_NAME } from '../../utils/config'
-import { WriteHandler } from './writeOperations/writeHandler'
-import { getFieldsOfTables } from '../../utils/db'
-import { ActionType } from './writeOperations/types'
+import { EditDataType, RegionDetails } from '../../../../frontend/src/shared/types'
+import Prisma from '../../../prisma/generated/now_test_client'
+import { nowDb } from '../../utils/db'
 
-const getRegionWriteHandler = (type: ActionType) => {
-  return new WriteHandler({
-    dbName: NOW_DB_NAME,
-    table: 'now_reg_coord',
-    idColumn: 'reg_coord_id',
-    allowedColumns: getFieldsOfTables(['now_reg_coord']),
-    type: type,
-  })
-}
+export const writeRegion = async (region: EditDataType<RegionDetails>) => {
+  let regionId: number
 
-export const writeRegion = async (
-  region: EditDataType<RegionDetails>,
-  comment: string | undefined,
-  references: Reference[] | undefined,
-  user: User | undefined
-) => {
-  const authorizer = user!.initials
-  const writeHandler = getRegionWriteHandler('update') // hardcoded for now to avoid issues
-  try {
-    await writeHandler.start()
-
-    if (!region.reg_coord_id) {
-      console.log('No creating new regions for now')
-      return false
-    } else {
-      await writeHandler.updateObject('now_reg_coord', region, ['reg_coord_id'])
-    }
-
-    writeHandler.idValue = region.reg_coord_id
-    await writeHandler.commit()
-    await writeHandler.end()
-    //await writeHandler.logUpdatesAndComplete(authorizer, comment ?? '', references ?? [])
-    return region.reg_coord_id
-  } catch (e) {
-    await writeHandler.end()
-    throw e
+  if (!region.reg_coord_id) {
+    const newRegion = await nowDb.now_reg_coord.create({
+      data: region as Prisma.now_reg_coord,
+    })
+    regionId = newRegion.reg_coord_id
+  } else {
+    await nowDb.now_reg_coord.update({
+      where: { reg_coord_id: region.reg_coord_id },
+      data: region as Prisma.now_reg_coord,
+    })
+    regionId = region.reg_coord_id
   }
+
+  return regionId
 }
--- frontend/src/shared/validators/region.ts
+++ frontend/src/shared/validators/region.ts
@@ -3,6 +3,10 @@ import { Validators, validator } from './validator'
 
 export const validateRegion = (editData: EditDataType<RegionDetails>, fieldName: keyof RegionDetails) => {
   const validators: Validators<EditDataType<Partial<RegionDetails>>> = {
+    reg_coord_id: {
+      name: 'reg_coord_id',
+      required: true,
+    },
     region: {
       name: 'Region',
       required: true,
