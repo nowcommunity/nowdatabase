50a0e85d763bfd35c3ad4ebaf7f296018a83f5a7
juhanikat <juhani.kataja@gmail.com>
2025-01-30 13:43:32 +0200

    contact form now autofills user information if it exists, also refactored ContactForm

--- frontend/src/components/DetailView/common/ContactForm.tsx
+++ frontend/src/components/DetailView/common/ContactForm.tsx
@@ -2,37 +2,45 @@ import { ContactModal } from './ContactModal'
 import { useDetailContext } from '../Context/DetailContext'
 import { usePageContext } from '@/components/Page'
 import { useEmailMutation } from '@/redux/emailReducer'
-import { useForm, Controller } from 'react-hook-form'
-import { Box, TextField, FormControlLabel, Switch } from '@mui/material'
+import { useForm, Controller, UseFormSetValue } from 'react-hook-form'
+import { Box, TextField, FormControlLabel, CircularProgress, Switch } from '@mui/material'
 import { useUser } from '@/hooks/user'
 import { useEffect } from 'react'
 import { useNotify } from '@/hooks/notification'
+import { useGetPersonDetailsQuery } from '@/redux/personReducer'
+import { skipToken } from '@reduxjs/toolkit/query'
 
 const formatContactEmail = (values: ContactFormValues) => {
   return `
   Subject: ${values.subject}\n
-  Name: ${values.username}\n
+  Name: ${values.full_name}\n
   Email: ${values.email}\n
-  Comments: ${values.comments}\n
-  Fast Reply Requested: ${values.fast_reply_requested ? 'Yes' : 'No'}
+  Fast Reply Requested: ${values.fast_reply_requested ? 'Yes' : 'No'} \n
+  Message: \n ${values.comments}
   `
 }
 
 type ContactFormValues = {
   subject: string
-  comments: string
-  username: string
+  full_name: string
   email: string
   fast_reply_requested: boolean
+  comments: string
 }
-export const ContactForm = <T extends object>({ buttonText }: { buttonText: string }) => {
-  const { data } = useDetailContext<T>()
-  const { createTitle } = usePageContext<T>()
+
+export const ContactForm = <T extends object>({
+  buttonText,
+  noContext,
+}: {
+  buttonText: string
+  noContext?: boolean
+}) => {
   const [sendEmailMutation, { isSuccess, isError }] = useEmailMutation()
-  const { register, trigger, getValues, control, formState } = useForm<ContactFormValues>()
+  const { trigger, getValues, register, setValue, control, formState } = useForm<ContactFormValues>()
   const { errors } = formState
   const notify = useNotify()
   const user = useUser()
+  const { data: personData, isLoading: personQueryloading } = useGetPersonDetailsQuery(user.initials ?? skipToken)
 
   const onSend = async () => {
     const result = await trigger()
@@ -56,21 +64,17 @@ export const ContactForm = <T extends object>({ buttonText }: { buttonText: stri
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSuccess, isError])
 
-  const subject = createTitle(data)
+  if (personQueryloading) return <CircularProgress />
 
   const form = (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
-      All fields need to be filled.
-      <br></br>
-      Enter your comments here
-      <br></br>
       <TextField
         sx={{ width: '25em' }}
         key="subject"
         label="Subject"
-        defaultValue={subject.length > 0 ? subject : ''}
+        defaultValue=""
         fullWidth
-        {...register('subject', { required: true })}
+        {...register('subject', { required: true, validate: value => !!value.trim() })}
         error={!!errors['subject']}
       />
       <br></br>
@@ -82,7 +86,7 @@ export const ContactForm = <T extends object>({ buttonText }: { buttonText: stri
         fullWidth
         multiline
         minRows={5}
-        {...register('comments', { required: true })}
+        {...register('comments', { required: true, validate: value => !!value.trim() })}
         error={!!errors['comments']}
       />
       <br></br>
@@ -90,20 +94,20 @@ export const ContactForm = <T extends object>({ buttonText }: { buttonText: stri
       <br></br>
       <TextField
         sx={{ width: '25em' }}
-        key="username"
-        label="Username"
-        defaultValue={user && user.username ? user.username : ''}
+        key="full_name"
+        label="Full Name"
+        defaultValue={personData?.full_name ?? ''}
         fullWidth
-        {...register('username', { required: true })}
-        error={!!errors['username']}
+        {...register('full_name', { required: true, validate: value => !!value.trim() })}
+        error={!!errors['full_name']}
       />
       <br></br>
       <TextField
         sx={{ width: '25em' }}
         key="email"
         label="Email"
-        defaultValue=""
-        {...register('email', { required: true })}
+        defaultValue={personData?.email ?? ''}
+        {...register('email', { required: true, validate: value => !!value.trim() })}
         error={!!errors['email']}
       />
       <Controller
@@ -119,100 +123,43 @@ export const ContactForm = <T extends object>({ buttonText }: { buttonText: stri
       />
     </Box>
   )
+
+  if (noContext) return <ContactFormTableView buttonText={buttonText} form={form} onSend={onSend} />
+  return <ContactFormDetailView<T> buttonText={buttonText} form={form} setValue={setValue} onSend={onSend} />
+}
+
+const ContactFormDetailView = <T extends object>({
+  buttonText,
+  onSend,
+  form,
+  setValue,
+}: {
+  buttonText: string
+  onSend: () => Promise<boolean>
+  form: JSX.Element
+  setValue: UseFormSetValue<ContactFormValues>
+}) => {
+  const { data } = useDetailContext<T>()
+  const { createTitle } = usePageContext<T>()
+
+  useEffect(() => {
+    // autofills the subject field on first render
+    const subject = createTitle(data)
+    setValue('subject', subject)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return <ContactModal {...{ buttonText, onSend }}>{form}</ContactModal>
 }
 
-// This is the same as ContactForm but it doesn't have context hooks, since table views do not have a context
-export const ContactFormTableView = ({ buttonText }: { buttonText: string }) => {
-  const [sendEmailMutation, { isSuccess, isError }] = useEmailMutation()
-  const { register, trigger, getValues, control, formState } = useForm<ContactFormValues>()
-  const { errors } = formState
-  const notify = useNotify()
-  const user = useUser()
-
-  const onSend = async () => {
-    const result = await trigger()
-    if (!result) return false
-    const values = getValues()
-    const title = values.subject
-    const message = formatContactEmail(values)
-
-    const confirm = window.confirm(`Send email?`)
-    if (!confirm) return false
-    void sendEmailMutation({ title, message })
-    return true
-  }
-
-  useEffect(() => {
-    if (isSuccess) {
-      notify('Email sent.')
-    } else if (isError) {
-      notify('Could not send email. Error happened.', 'error')
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSuccess, isError])
-
-  const form = (
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
-      All fields need to be filled for the email to get sent. Note: currently sends emails to a testing account.
-      <br></br>
-      Enter your comments here<br></br>
-      <TextField
-        sx={{ width: '25em' }}
-        key="subject"
-        label="Subject"
-        defaultValue={''}
-        fullWidth
-        {...register('subject', { required: true })}
-        error={!!errors['subject']}
-      />
-      <br></br>
-      <TextField
-        sx={{ width: '40em' }}
-        key="comments"
-        label="Comments"
-        defaultValue=""
-        fullWidth
-        multiline
-        minRows={5}
-        {...register('comments', { required: true })}
-        error={!!errors['comments']}
-      />
-      <br></br>
-      Your contact information
-      <br></br>
-      <TextField
-        sx={{ width: '25em' }}
-        key="username"
-        label="Name"
-        defaultValue={user && user.username ? user.username : ''}
-        fullWidth
-        {...register('username', { required: true })}
-        error={!!errors['username']}
-      />
-      <br></br>
-      <TextField
-        sx={{ width: '25em' }}
-        key="email"
-        label="Email"
-        defaultValue=""
-        {...register('email', { required: true })}
-        error={!!errors['email']}
-      />
-      <Controller
-        control={control}
-        name="fast_reply_requested"
-        defaultValue={false}
-        render={({ field: { onChange, value } }) => (
-          <FormControlLabel
-            control={<Switch checked={value} onChange={onChange} />}
-            label="Please contact me as soon as possible."
-          />
-        )}
-      />
-    </Box>
-  )
-
+const ContactFormTableView = ({
+  buttonText,
+  form,
+  onSend,
+}: {
+  buttonText: string
+  form: JSX.Element
+  onSend: () => Promise<boolean>
+}) => {
   return <ContactModal {...{ buttonText, onSend }}>{form}</ContactModal>
 }
