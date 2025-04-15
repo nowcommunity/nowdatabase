7e35f614172db5a270c31da229c113426361b573
juhanikat <juhani.kataja@gmail.com>
2025-04-15 16:06:51 +0300

    add comment to fixbigint

--- backend/src/utils/common.ts
+++ backend/src/utils/common.ts
@@ -1,5 +1,6 @@
 export const sleep = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
+// Note: using fixBigInt inside an object might crash the entire app for some reason
 export const fixBigInt = (obj: object | null) =>
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   obj === null ? null : JSON.stringify(obj, (_key, value) => (typeof value === 'bigint' ? Number(value) : value))
