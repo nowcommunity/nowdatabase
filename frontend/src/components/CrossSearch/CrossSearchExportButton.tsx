b1c61023f060f013db68cc536173f7f8a2c2e11c
juhanikat <juhani.kataja@gmail.com>
2025-04-16 16:54:59 +0300

    added column filtering and sorting to export, and fixed bug with crosssearch/all route

--- frontend/src/components/CrossSearch/CrossSearchExportButton.tsx
+++ frontend/src/components/CrossSearch/CrossSearchExportButton.tsx
@@ -3,9 +3,11 @@ import { useNotify } from '@/hooks/notification'
 import { BACKEND_URL } from '@/util/config'
 import { Box, Button, CircularProgress } from '@mui/material'
 import { useUser } from '@/hooks/user'
+import { usePageContext } from '../Page'
 
 export const CrossSearchExportButton = () => {
   const [loading, setLoading] = useState(false)
+  const { sqlColumnFilters, sqlOrderBy } = usePageContext()
   const notify = useNotify()
 
   const token = useUser().token
@@ -15,7 +17,9 @@ export const CrossSearchExportButton = () => {
     setLoading(true)
     notify('Download started, please wait...')
 
-    fetch(`${BACKEND_URL}/crosssearch/export`, { headers: headers })
+    fetch(`${BACKEND_URL}/crosssearch/export/${JSON.stringify(sqlColumnFilters)}/${JSON.stringify(sqlOrderBy)}`, {
+      headers: headers,
+    })
       .then(response => {
         if (!response.ok) {
           throw new Error('Server response was not OK.')
