4965138cd04e624d2cb424e7ad842057ba53c682
juhanikat <juhani.kataja@gmail.com>
2025-05-09 14:56:36 +0300

    added new test reference to anon database and made a deletion test

--- cypress/e2e/reference.cy.js
+++ cypress/e2e/reference.cy.js
@@ -9,7 +9,7 @@ describe('Creating a reference', () => {
 
   it.skip('with valid data works', () => {})
 
-  it.skip('with missing required fields does no work', () => {})
+  it.skip('with missing required fields does not work', () => {})
 
   // TODO: add more tests for different types of references etc.
 })
@@ -21,7 +21,7 @@ describe('Editing a reference', () => {
 
   it.skip('with valid data works', () => {})
 
-  it.skip('with missing required fields does no work', () => {})
+  it.skip('with missing required fields does not work', () => {})
 
   // TODO: add more tests for different types of references etc.
 })
@@ -31,9 +31,8 @@ describe('Deleting a reference', () => {
     cy.login('testSu')
   })
 
-  it.skip('that is not linked to anything works and returns user to table view', () => {
-    // TODO: Create a new reference in anon database that is not linked to any localities or species
-    cy.visit('/reference/<reference_id>')
+  it('that is not linked to anything works and returns user to table view', () => {
+    cy.visit('/reference/25000')
     cy.get('[id=delete-button]').should('exist').click()
     cy.on('window:confirm', str => {
       expect(str).to.eq('Are you sure you want to delete this item? This operation cannot be undone.')
@@ -41,12 +40,12 @@ describe('Deleting a reference', () => {
     })
     cy.contains('Deleted item successfully.')
     cy.contains('Dated co-occurrence of Homo erectus and Gigantopithecus from Tham Khuyen Cave, Vietnam') // check that table view has been navigated to
-    cy.contains('<Deleted reference title>').should('not.exist')
-    cy.visit('/reference/<reference_id>')
+    cy.contains('A test reference that is not linked to any localities or species').should('not.exist')
+    cy.visit('/reference/25000')
     cy.contains('Error loading data')
   })
 
-  // Skipped because referecnes that have linked localities or species cannot be deleted currently.
+  // Skipped because references that have linked localities or species cannot be deleted currently
   it.skip('that is linked to a locality and a species works and returns user to table view', () => {
     cy.visit('/reference/10039')
     cy.get('[id=delete-button]').should('exist').click()
