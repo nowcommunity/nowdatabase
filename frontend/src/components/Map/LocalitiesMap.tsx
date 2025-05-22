67a3f11bc039f4b0dbb354a76b6339ea6c796496
Elias Junno <junnoelias@gmail.com>
2025-05-22 12:52:26 +0300

    add fetch and store locality details in frontend

--- frontend/src/components/Map/LocalitiesMap.tsx
+++ frontend/src/components/Map/LocalitiesMap.tsx
@@ -1,15 +1,26 @@
-import { useGetAllLocalitiesQuery } from '../../redux/localityReducer'
-import { useEffect, useRef } from 'react'
+import { useGetAllLocalitiesQuery, useGetLocalityDetailsQuery } from '../../redux/localityReducer'
+import { useState, useEffect, useRef } from 'react'
 import 'leaflet/dist/leaflet.css'
-import L, { LatLngExpression } from 'leaflet'
+import L, { LatLngExpression, Marker } from 'leaflet'
+
+type CustomMarkerOptions = L.MarkerOptions & { localityId: number }
+type CustomMarker = L.Marker & { options: CustomMarkerOptions }
 
 export const LocalitiesMap = () => {
+  // use null instead?
+  const [selectedLocality, setSelectedLocality] = useState<number>(0)
   const mapRef = useRef<HTMLDivElement | null>(null)
+  const leafletMapRef = useRef<L.Map | null>(null)
   const { data: localitiesQueryData, isFetching: localitiesQueryIsFetching } = useGetAllLocalitiesQuery()
 
-  useEffect(() => {
-    if (!mapRef.current || localitiesQueryIsFetching) return
+  const {
+    data: localityDetailsQueryData,
+    isFetching: detailsLoading,
+    error,
+  } = useGetLocalityDetailsQuery(selectedLocality?.toString(), { skip: !selectedLocality })
 
+  useEffect(() => {
+    if (!mapRef.current || leafletMapRef.current) return
     const map = L.map(mapRef.current)
     const coords: LatLngExpression = [60.204637, 24.962123]
 
@@ -20,13 +31,39 @@ export const LocalitiesMap = () => {
       noWrap: true,
     }).addTo(map)
 
-    localitiesQueryData?.forEach(locality =>
-      L.marker([locality.dec_lat, locality.dec_long]).addTo(map).bindPopup(locality.loc_name)
-    )
+    leafletMapRef.current = map
+
     return () => {
       map.remove()
+      leafletMapRef.current = null
     }
-  }, [localitiesQueryData, localitiesQueryIsFetching])
+  }, [])
+
+  useEffect(() => {
+    if (!leafletMapRef.current || !localitiesQueryData || localitiesQueryIsFetching) return
+
+    localitiesQueryData.forEach(locality => {
+      const options: CustomMarkerOptions = {
+        title: locality.loc_name,
+        localityId: locality.lid,
+      }
+
+      const marker = L.marker([locality.dec_lat, locality.dec_long], options) as CustomMarker
+      marker.addTo(leafletMapRef.current!).bindPopup(locality.loc_name)
+
+      marker.on('click', () => {
+        setSelectedLocality(marker.options.localityId)
+        console.log('Clicked marker with ID:', marker.options.localityId)
+        console.log('State updated with locality ID:', selectedLocality)
+      })
+    })
+  }, [localitiesQueryData, localitiesQueryIsFetching, selectedLocality])
+
+  useEffect(() => {
+    if (localityDetailsQueryData) {
+      console.log('Fetched locality details:', localityDetailsQueryData)
+    }
+  })
 
   document.title = 'Map'
 
