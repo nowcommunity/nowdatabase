7d55f2c6fabae3958302c984b1435d015f700654
Tepponen Margit H <mateppon@cubbli24-gold.cs.helsinki.fi>
2025-06-25 09:33:03 +0300

    add validation for persons fields

--- frontend/src/shared/validators/person.ts
+++ frontend/src/shared/validators/person.ts
@@ -1,6 +1,7 @@
 import { EditDataType, PersonDetailsType } from '../types'
 import { Validators, validator } from './validator'
 
+
 export const validatePerson = (
   editData: EditDataType<PersonDetailsType>,
   fieldName: keyof EditDataType<PersonDetailsType>
@@ -9,27 +10,44 @@ export const validatePerson = (
     initials: {
       name: 'initials',
       required: true,
-    },
+      asString: true,
+      minLength: 2,
+      maxLength: 8,
+  },
     first_name: {
       name: 'First Name',
       required: true,
+      asString: true,
+      minLength: 2,
+      maxLength: 20,
+
     },
     surname: {
       name: 'Surname',
       required: true,
+      asString: true,
+      minLength: 2,
+      maxLength: 20,
     },
-    email: {
+    email: { 
       name: 'Email',
       required: true,
+      asString: true,
+      minLength: 5,
+      maxLength: 30,
     },
     organization: {
       name: 'Organization',
       required: true,
+      asString: true,
+      minLength: 2,
+      maxLength: 30,
     },
     country: {
       name: 'Country',
       required: true,
     },
+
   }
 
   return validator<EditDataType<PersonDetailsType>>(validators, editData, fieldName)
