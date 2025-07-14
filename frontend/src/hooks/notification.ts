bb859d8b1333871b99c9d54b12a009bd82ca1409
juhanikat <juhani.kataja@gmail.com>
2025-07-14 12:33:54 +0300

    add comment to notification.ts

--- frontend/src/hooks/notification.ts
+++ frontend/src/hooks/notification.ts
@@ -3,5 +3,7 @@ import { useContext } from 'react'
 
 export const useNotify = () => {
   const { notify, setMessage } = useContext(NotificationContext)
+  // setMessage can be used to change a notification's message without creating a new one
+  // use this to create a progress bar etc.
   return { notify, setMessage }
 }
