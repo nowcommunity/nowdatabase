995b50f2fbaea8cb489a3c42fb20b0dd5fb666cd
juhanikat <juhani.kataja@gmail.com>
2025-07-28 20:54:22 +0300

    add create tests for museum

---
+++ backend/src/api-tests/museum/data.ts
@@ -0,0 +1,14 @@
+import { Museum } from '../../../../frontend/src/shared/types'
+
+export const newMuseumBasis: Museum = {
+  museum: 'AM',
+  institution: 'Australian Museum',
+  alt_int_name: '',
+  city: 'Sydney',
+  state_code: '',
+  state: '',
+  country: 'Australia',
+  used_morph: true,
+  used_now: null,
+  used_gene: null,
+}
