d302677ca41eff09004b48ae69e49a95dd3b8977
juhanikat <juhani.kataja@gmail.com>
2025-08-25 17:03:51 +0300

    add warning for users when creating a new museum

--- frontend/src/components/Museum/Tabs/MuseumInfoTab.tsx
+++ frontend/src/components/Museum/Tabs/MuseumInfoTab.tsx
@@ -1,10 +1,25 @@
 import { ArrayFrame } from '@/components/DetailView/common/tabLayoutHelpers'
-import { useDetailContext } from '@/components/DetailView/Context/DetailContext'
+import { modeOptionToMode, useDetailContext } from '@/components/DetailView/Context/DetailContext'
+import { useNotify } from '@/hooks/notification'
 import { Museum } from '@/shared/types'
 import { validCountries } from '@/shared/validators/countryList'
+import { useEffect } from 'react'
 
 export const MuseumInfoTab = ({ isNew }: { isNew?: boolean }) => {
-  const { textField, dropdownWithSearch } = useDetailContext<Museum>()
+  const { textField, dropdownWithSearch, mode } = useDetailContext<Museum>()
+  const { notify } = useNotify()
+
+  useEffect(() => {
+    if (mode === modeOptionToMode['new']) {
+      notify(
+        "Due to current limitations, using an existing museum's code will overwrite that museum with the one you are currently creating. \n \
+        Check that you are using a unique museum code.",
+        'warning',
+        null
+      )
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mode])
 
   const countryOptions = ['', ...validCountries]
 
