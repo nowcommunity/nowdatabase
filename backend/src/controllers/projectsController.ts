f77b6522b9f64628d18342722fd0ca10c5a35be2
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-12-03 15:12:20 +0200

    Support project updates for coordinator and members

--- backend/src/controllers/projectsController.ts
+++ backend/src/controllers/projectsController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express'
-import { createProject, CreateProjectInput } from '../services/projectsService'
+import { createProject, CreateProjectInput, updateProject, UpdateProjectInput } from '../services/projectsService'
 
 export const createProjectHandler = async (req: Request<object, object, CreateProjectInput>, res: Response) => {
   const createdProject = await createProject(req.body)
 
   return res.status(201).json(createdProject)
+}
+
+export const updateProjectHandler = async (req: Request<{ id: string }, object, UpdateProjectInput>, res: Response) => {
+  const updatedProject = await updateProject(Number(req.params.id), req.body)
+
+  return res.status(200).json(updatedProject)
 }
