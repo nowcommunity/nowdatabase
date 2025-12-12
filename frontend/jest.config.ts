f72b91a399c6ab150c2e7375ee044f01afdde102
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-12-12 13:30:06 +0200

    Fix TableHelp alias path

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -9,7 +9,7 @@ export default {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@components/table$': '<rootDir>/src/components/table',
+    '^@components/Table$': '<rootDir>/src/components/table',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@util/(.*)$': '<rootDir>/src/util/$1',
     '^@shared/(.*)$': '<rootDir>/shared/$1',
