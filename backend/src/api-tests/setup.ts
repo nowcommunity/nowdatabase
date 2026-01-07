6dfca66107d46fd16170e1fce1db8a98dd9e1afe
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2026-01-07 17:59:43 +0200

    Update setup.ts to disconnect testPrisma connection after all tests

--- backend/src/api-tests/setup.ts
+++ backend/src/api-tests/setup.ts
@@ -1,7 +1,9 @@
 import { afterAll } from '@jest/globals'
 import { pool, nowDb, logDb } from '../utils/db'
+import { testPrisma } from './helpers/prisma'
 
 afterAll(async () => {
+  await testPrisma.$disconnect()
   await pool.end()
   await nowDb.$disconnect()
   await logDb.$disconnect()
