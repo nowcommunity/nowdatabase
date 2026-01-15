77418e127cfc1afd252d45d767733c65e9eedddb
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2026-01-15 15:02:59 +0200

    Format backend Jest config

--- backend/jest-config.js
+++ backend/jest-config.js
@@ -1,17 +1,6 @@
 module.exports = {
-    testPathIgnorePatterns: [
-        "<rootDir>/build",
-        "<rootDir>/node_modules",
-    ],
-    collectCoverageFrom: [
-        "./src/**",
-        "!./src/api-tests/**"
-    ],
-    roots: [
-        "./src"
-    ],
-    coverageReporters: [
-        "text",
-        "lcov"
-    ]
+  testPathIgnorePatterns: ["<rootDir>/build", "<rootDir>/node_modules", "<rootDir>/../src/api-tests"],
+  collectCoverageFrom: ["./src/**", "!./src/api-tests/**"],
+  roots: ["./src"],
+  coverageReporters: ["text", "lcov"],
 }
