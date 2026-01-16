7c4db55e170363890f212db1d2a2a580ba2e1e37
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2026-01-16 11:46:32 +0200

    Handle project validation errors

--- backend/src/controllers/projectsController.ts
+++ backend/src/controllers/projectsController.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express'
 import { createProject, CreateProjectInput, updateProject, UpdateProjectInput } from '../services/projectsService'
+import { NotFoundError, ValidationError } from '../validators/projectsValidator'
 
 export const createProjectHandler = async (req: Request<object, object, CreateProjectInput>, res: Response) => {
-  const createdProject = await createProject(req.body)
-
-  return res.status(201).json(createdProject)
+  try {
+    const createdProject = await createProject(req.body)
+    return res.status(201).json(createdProject)
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return res.status(error.status).json({ message: error.message })
+    }
+    throw error
+  }
 }
 
 export const updateProjectHandler = async (req: Request<{ id: string }, object, UpdateProjectInput>, res: Response) => {
-  const updatedProject = await updateProject(Number(req.params.id), req.body)
-
-  return res.status(200).json(updatedProject)
+  try {
+    const updatedProject = await updateProject(Number(req.params.id), req.body)
+    return res.status(200).json(updatedProject)
+  } catch (error) {
+    if (error instanceof ValidationError || error instanceof NotFoundError) {
+      return res.status(error.status).json({ message: error.message })
+    }
+    throw error
+  }
 }
