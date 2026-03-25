dc27e545b1331275236c0163bb11dba53d910913
karilint <kari.lintulaakso@gmail.com>
2026-03-25 14:50:24 +0200

    Relax admin Cypress smoke assertions for slow loads

--- cypress/e2e/asAdmin.cy.js
+++ cypress/e2e/asAdmin.cy.js
@@ -2,6 +2,8 @@ before('Reset database', () => {
   cy.resetDatabase()
 })
 
+const pageLoadTimeout = 30000
+
 describe('Open each page, table view and detail view, and check at least some correct text appears', () => {
   beforeEach('Login as admin', () => {
     cy.login('testSu')
@@ -9,36 +11,39 @@ describe('Open each page, table view and detail view, and check at least some co
 
   it('Locality works', () => {
     cy.visit('/locality')
-    cy.contains('Name').should('be.visible')
-    cy.contains('Country').should('be.visible')
+    cy.contains('Name', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.contains('Country', { timeout: pageLoadTimeout }).should('be.visible')
     cy.visit('/locality/21050')
-    cy.contains('Dmanisi')
-    cy.contains('Dating method')
-    cy.contains('Lithology')
-    cy.contains('olduvai')
+    cy.contains('Dmanisi', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.contains('Dating method', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.contains('Lithology', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.contains('olduvai', { timeout: pageLoadTimeout }).should('be.visible')
   })
 
   it('Species works', () => {
     cy.visit('/species')
     cy.location('pathname').should('eq', '/species')
-    cy.visit('/species/21052')
-    cy.contains('Class')
-    cy.contains('Simplomys')
+    cy.contains('Order', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.visit('/species/21052/')
+    cy.contains('21052 Simplomys simplicidens', { timeout: pageLoadTimeout }).should('be.visible')
   })
 
   it('Reference works', () => {
     cy.visit('/reference')
     cy.location('pathname').should('eq', '/reference')
+    cy.contains('References', { timeout: pageLoadTimeout }).should('be.visible')
     cy.visit('/reference/10039')
-    cy.contains('Reference type')
-    cy.contains('A new geomagnetic polarity time scale for the Late Cretaceous and Cenozoic')
+    cy.contains('Reference type', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.contains('A new geomagnetic polarity time scale for the Late Cretaceous and Cenozoic', {
+      timeout: pageLoadTimeout,
+    }).should('be.visible')
   })
 
   it('Time Unit works', () => {
     cy.visit('/time-unit')
     cy.location('pathname').should('eq', '/time-unit')
-    cy.visit('/time-unit/langhian')
-    cy.contains('Sequence')
-    cy.contains('GCSS')
+    cy.contains('Sequence', { timeout: pageLoadTimeout }).should('be.visible')
+    cy.visit('/time-unit/bahean?tab=0')
+    cy.contains('Bahean', { timeout: pageLoadTimeout }).should('be.visible')
   })
 })
