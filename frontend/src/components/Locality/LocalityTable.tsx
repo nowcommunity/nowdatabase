9b98a91ee4a2b8ce9c8be02b120d7586221d3e65
juhanikat <juhani.kataja@gmail.com>
2026-06-30 13:30:15 +0300

    fix menuitems not showing in table export menus, and give them keys (#1214)

--- frontend/src/components/Locality/LocalityTable.tsx
+++ frontend/src/components/Locality/LocalityTable.tsx
@@ -522,7 +522,9 @@ export const LocalityTable = ({ selectorFn }: { selectorFn?: (newObject: Localit
         url="locality"
         kmlExport={kmlExport}
         svgExport={svgExport}
-        renderExtraExportMenuItems={[handleClose => <LocalityDwcExportMenuItem handleClose={handleClose} />]}
+        renderExtraExportMenuItems={[
+          (handleClose, key) => <LocalityDwcExportMenuItem handleClose={handleClose} key={key} />,
+        ]}
         enableColumnFilterModes={true}
         tableRowAction={handleLocalityRowActionClick}
       />
