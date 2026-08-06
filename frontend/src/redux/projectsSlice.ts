9ba304798e81aa6f4fcad036926eca71d8bb9917
juhanikat <juhani.kataja@gmail.com>
2026-08-06 15:10:50 +0300

    delete files

--- frontend/src/redux/projectsSlice.ts
+++
@@ -1,8 +0,0 @@
-export type { CreateProjectPayload, UpdateProjectPayload } from './projectReducer'
-export {
-  useGetAllProjectsQuery,
-  useGetProjectDetailsQuery,
-  useCreateProjectMutation,
-  useUpdateProjectMutation,
-  useDeleteProjectMutation,
-} from './projectReducer'
