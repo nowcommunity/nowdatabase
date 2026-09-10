7cc58856b2a68e0eaf5b055d073f6c489bd4c3f1
juhanikat <juhani.kataja@gmail.com>
2026-09-10 14:14:17 +0300

    add species unique identifier to occurrence details

--- frontend/src/components/Occurrence/Tabs/OccurrenceCoreTab.tsx
+++ frontend/src/components/Occurrence/Tabs/OccurrenceCoreTab.tsx
@@ -94,6 +94,7 @@ export const OccurrenceCoreTab = ({ existingOccurrences }: { existingOccurrences
               ['Family', editData.family_name],
               ['Genus', editData.genus_name],
               ['Species', editData.species_name],
+              ['Unique Identifier', editData.unique_identifier],
               ['ID status', dropdown('id_status', idStatusOptions, 'ID status')],
               ['Additional Information', textField('orig_entry', { type: 'text' })],
               ['Source name', textField('source_name', { type: 'text' })],
